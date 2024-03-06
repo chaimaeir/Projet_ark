d2091dfd8d1f05da856aecc255e242a657412268
Ouuuumyy <125665818+Ouuuumyy@users.noreply.github.com>
2024-03-06 14:30:12 +0100

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,1 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const db = process.env.DATABASE_URL
 
-mongoose.connect(db)
-.then(()=>console.log('connected to MongoDB'))
-.catch((err)=>{
-    console.error("failed to connect to MongoDB", err)
-    process.exit(1)});
