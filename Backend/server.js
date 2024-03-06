b7c37fe80c952cb234576159dddd486e56c3bfd4
Ouuuumyy <125665818+Ouuuumyy@users.noreply.github.com>
2024-03-06 14:29:45 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const app = express();
 
-const bodyparser = require('body-parser');
-const productRoute = require('./routes/productRoutes');
-require('./config/db');
-
-app.use(express.json());
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
-});
-
-app.use('/products', productRoute);
