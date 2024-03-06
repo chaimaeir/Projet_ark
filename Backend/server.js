7f1dd73652e880b2f6aebb86662a6242f27dbbb6
chaimaeir <iraamane20@gmail.com>
2024-03-06 15:25:45 -0800

    commit change

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const  app = express();
 app.use(express.json());
 
 app.use('/users',userRoute)
-app.use('/products',productRoute)
+//app.use('/products',productRoute)
 
 const port= process.env.PORT ||3000
 
