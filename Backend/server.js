8e102cd1b4039fea3c08a99bf8a1028a609f08d0
Ouuuumyy <125665818+Ouuuumyy@users.noreply.github.com>
2024-03-06 14:41:03 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,20 @@
+const express=require( 'express')
+ require('./config/db');
 
+const userRoute= require('./routes/userRoutes');
+const productRoute = require('./routes/productRoutes');
+
+
+
+
+
+const  app = express();
+app.use(express.json());
+
+app.use('/users',userRoute)
+app.use('/products' ,productsRoute)
+
+
+const port= process.env.PORT ||3000
+
+app.listen(port, ()=>{console.log(`Server is running on ${port}`)})
