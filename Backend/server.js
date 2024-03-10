8618e8fd0e44a630f703ffc5f1755abd204a95ec
chaimaeir <iraamane20@gmail.com>
2024-03-10 22:27:13 +0000

    some modification

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-const port=process.env.PORT || 3000
+const port=process.env.PORT 
 
 
 app.listen(port, ()=>{console.log(`Server is running on ${port}`)})
